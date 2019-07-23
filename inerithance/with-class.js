88946eb9d437f5d74d3b9e30a7f065e7217e912c
king-prawns <giorgio.gamberoni@gmail.com>
2019-07-23 12:18:07 +0200

    update with-class js

--- inerithance/with-class.js
+++ inerithance/with-class.js
@@ -18,4 +18,35 @@ class Person {
   }
 }
 
-let han = new Person("Han", "Solo", 25, "male", ["Smuggling"]);
+let person1 = new Person("Han", "Solo", 25, "male", ["Smuggling"]);
+
+class Teacher extends Person {
+  constructor(first, last, age, gender, interests, subject, grade) {
+    super(first, last, age, gender, interests);
+    // subject and grade are specific to Teacher
+    this._subject = subject;
+    this.grade = grade;
+  }
+
+  get subject() {
+    return this._subject;
+  }
+
+  set subject(newSubject) {
+    this._subject = newSubject;
+  }
+
+  farewell() {
+    alert(`${this.name.first} has left`);
+  }
+}
+
+let teacher1 = new Teacher(
+  "Severus",
+  "Snape",
+  58,
+  "male",
+  ["Potions"],
+  "Dark arts",
+  5
+);
